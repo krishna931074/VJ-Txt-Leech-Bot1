bf49dbfdcebf7402769e610f67052039f6774a0f
krishna931074 <kumarrohit92572@gmail.com>
2025-01-12 14:34:21 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,9 +1,9 @@
-# Don't Remove Credit Tg - @SONICKUWALSSCBOT
-# website For Amazing Bot https://sonickuwalssc.blogspot.com/
-# Ask Doubt on telegram @SONICKUWALUPDATEKANHA
+# Don't Remove Credit Tg - @Strangerboy27_bot_strangerboy
+# website For Amazing Bot https://envs.sh/vb_.jpg
+# Ask Doubt on telegram @Strangerboy27_bot_strangerboy
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+API_ID = int(environ.get("API_ID", "21567814"))
+API_HASH = environ.get("API_HASH", "cd7dc5431d449fd795683c550d7bfb7e")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
