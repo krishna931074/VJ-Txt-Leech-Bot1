22466808a9d125de1304750e94987e5a884458d0
STRANGER BOY 💔 BROKEN 👦 <kumarrohit92572@gmail.com>
2025-01-21 16:57:33 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -6,4 +6,4 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "21567814"))
 API_HASH = environ.get("API_HASH", "cd7dc5431d449fd795683c550d7bfb7e")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7871119061:AAGajjkdcwUOcVstPEJJpTTA9L-5j3f2BFM")
